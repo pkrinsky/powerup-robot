b95f3d13f314c3c0c7bc8e9e6e95e879e1a4c48c
Paul Krinsky <paulkr@outlook.com>
2018-06-07 10:03:03 -0700

    Initial version

---
+++ src/powerup/team000/MyClient.java
@@ -0,0 +1,43 @@
+package powerup.team000;
+
+import powerup.engine.Util;
+import powerup.field.Robot;
+import powerup.server.GameClient;
+
+public class MyClient extends GameClient {
+	
+	// slow the game down a little to make it easier to watch
+	static int GAME_DELAY = 200;
+	
+	public static void main(String[] args) {
+		GameClient client = new MyClient();
+		String name = null;
+		String debug = null;
+		String serverAddress = null;
+		String serverPort = null;
+		
+		if (args.length >= 1) {
+			name = args[0];
+		}
+		if (args.length >= 2) {
+			debug = args[1];
+			Util.setDebugLevel(new Integer(debug));
+		}
+		if (args.length >= 3) {
+			serverAddress = args[2];
+		}
+		if (args.length >= 4) {
+			serverPort = args[3];
+		}
+			
+		client.setup(serverAddress, serverPort, name);
+		client.gameLoop(GAME_DELAY);
+	}	
+
+	@Override
+	protected Robot newRobot() {
+		Robot robot = new MyRobot();
+		return robot;
+	}
+
+}
---
+++ src/powerup/team000/MyRobot.java
@@ -0,0 +1,70 @@
+package powerup.team000;
+
+import powerup.field.Field;
+import powerup.field.FieldObject;
+import powerup.field.Robot;
+
+public class MyRobot extends Robot {
+	
+	/* 
+	 * Called periodically by the GameClient to get the next move
+	 */
+	public int move(Field field) {
+		// default next move to STOP
+		int nextMove = Robot.STOP;
+		
+		// controls which algorithm to use to control the robot
+		int algorithm = 2;
+
+		switch (algorithm) {
+		case 1:
+			// manually control the robot with the keyboard
+			nextMove = super.move(field);
+			break;
+		case 2:
+			// autonomous routine to move to the scale and shoot
+			nextMove = moveToScaleAndShoot(field);
+			break;
+		}
+
+		return nextMove;
+	}	
+
+	/* 
+	 * Determine the next move using basic positional logic
+	 */
+	private int moveToScaleAndShoot(Field field) {
+		// default next move to STOP
+		int nextMove = Robot.STOP;
+		
+		// get the current location of the robot
+		int currentCol = getCol();
+		int currentRow = getRow();
+		
+		// find the blue scale and move to just below its location
+		FieldObject blueScale = field.find("BS");
+		int targetCol = blueScale.getCol();
+		int targetRow = blueScale.getRow()+1;
+		
+		// compare the current row to the target to determine direction
+		if (currentRow > targetRow)
+			nextMove = Robot.NORTH;
+
+		// compare the current column to the target to determine direction
+		if (currentCol < targetCol)
+			nextMove = Robot.EAST;
+		
+		//TODO need to implement SOUTH and WEST
+		
+		// if we have arrived and have a cube then shoot
+		if (currentRow == targetRow 
+				&& currentCol == targetCol
+				&& hasCube()) {
+			nextMove = Robot.SHOOT;
+		}
+		
+		return nextMove;
+	}
+
+	
+}
